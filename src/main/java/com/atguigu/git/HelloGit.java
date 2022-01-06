14ef9d3fa6338e791c367b88df71f9568a5e2322
liyuan <861924298@qq.com>
2022-01-06 12:27:29 +0800

    克隆项目添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -14,6 +14,7 @@ public class HelloGit {
         System.out.println("主干添加1!");
         System.out.println("分支添加1!");
         System.out.println("在线更新!");
+        System.out.println("克隆项目添加!");
     }
 }
 
