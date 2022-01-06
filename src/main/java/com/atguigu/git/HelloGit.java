0b351edb230428ab6d7ede51f43c1e829dfb35e6
liyuan <41234874+liyuan-edu@users.noreply.github.com>
2022-01-06 12:13:03 +0800

    Update HelloGit.java
    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -13,6 +13,7 @@ public class HelloGit {
         System.out.println("创建分支!");
         System.out.println("主干添加1!");
         System.out.println("分支添加1!");
+        System.out.println("在线更新!");
     }
 }
 
